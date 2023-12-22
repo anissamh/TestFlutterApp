656499e2911c78c8b3928e63517ea786073c0f36
anissamh <an.infmhiri@gmail.com>
2023-12-22 10:55:08 +0100

    Premier commit

---
+++ Desktop/Faculté/master/2eme/perso/app testtechnique/apptest/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:apptest/onboarding.dart';
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static final String title = 'Onboarding Example';
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home:onBoarding(),
+        //DataPage(),
+       
+      );
+}
